c1219cb53fedfc20cbda62fe94cd93531d0f730b
Iga-Bubula <iga.bubula1@gmail.com>
2026-04-10 17:38:24 +0200

    add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+
 
 @Module({
-  imports: [],
+  imports: [UsersModule, TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'cinema',
+      entities: [
+        User
+      ],
+      synchronize: true,
+    }),],  
   controllers: [AppController],
   providers: [AppService],
 })
